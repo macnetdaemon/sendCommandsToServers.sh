737db2e094c5dfb01b7f2de37c9b17ec1e5fafe7
Mac <macnetdaemon@users.noreply.github.com>
2016-01-20 15:33:27 -0500

    Update sendCommandsToServers.sh

--- sendCommandsToServers.sh
+++ sendCommandsToServers.sh
@@ -1,4 +1,20 @@
 #/bin/bash
+# For this script to work you must setup your authorized ssh keys on all your remote servers.
+# Simple instructions: You many have to generate your keys with a command like: ssh-keygen -t dsa
+# If NO authorized_keys file exists in the account home dir/.ssh folder of your remote server:
+# 	copy your public key to your remote servers with scp:
+# 	scp ~/.ssh/id_dsa.pub your_admin_username@yourserver:.ssh/authorized_keys
+# If an authorized keys file already exists on your remote server:
+# 	you can copy your public key to the ~/.ssh folder as i.e. mykey.pub
+#	scp ~/.ssh/id_dsa.pub your_admin_username@yourserver:.ssh/mykey.pub
+#	append mykey.pub to authorized keys with: cat ~/.ssh/mykey.pub >> authorized_keys
+# Be sure to change your_admin_username@yourserver to the your credentials and server name.
+# You _may_ need to:
+# 	Connect via ssh into your server and go to ~/.ssh
+# 	Chown the authorized_keys file with your_admin_username: e.g. sudo chown admin authorized_keys
+# 	Update the permission: sudo chmod 755 authorized_keys
+#
+# If you run this on a Macintosh you can hardwire the username and password, the rename the script with the file extension .command in order to make it double-clickable.
 
 username=''
 password=''
